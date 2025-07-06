84ccfd9ffa7fd10c226209cf3fd96f663a0098cd
ilhamxkz <ilhamza70@gmail.com>
2025-07-06 21:23:06 +0700

    menammbahkan index.html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contoh 2</title>
+</head>
+<body>
+    <h3>lorem
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat veniam beatae perspiciatis perferendis repudiandae omnis inventore explicabo fugit quisquam magni, saepe nulla ipsum sunt minima iste nostrum. Nesciunt, omnis sint!
+    </h3>
+</body>
+</html>
